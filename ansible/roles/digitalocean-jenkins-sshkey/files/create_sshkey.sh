fd29d4e2591746103765eefe400f115ccb92fac6
Shohei Kameda <shoheik@cpan.org>
2014-03-06 08:04:15 +0900

    added jenkins sshkey to digital ocean

--- ansible/roles/digitalocean-jenkins-sshkey/files/create_sshkey.sh
+++ ansible/roles/digitalocean-jenkins-sshkey/files/create_sshkey.sh
@@ -2,8 +2,6 @@
 
 if [[ ! -e /var/lib/jenkins/.ssh ]]
 then
-    /usr/bin/su -s /bin/bash jenkins
-    /usr/bin/ssh-keygen -t rsa -N "" -f /var/lib/jenkins/.ssh/id_rsa
-    ../scripts/doman.pl --create_sshkey -sshkey_name jenkins -pub_sshkey /var/lib/jenkins/.ssh/id_rsa.pub
+    sudo -u jenkins /usr/bin/ssh-keygen -t rsa -N "" -f /var/lib/jenkins/.ssh/jenkins_id_rsa
 fi
 
