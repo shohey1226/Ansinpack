63c997e230dacc0b439217f07ed7a418bc252ce3
Shohei Kameda <shoheik@cpan.org>
2014-03-22 16:54:19 +0900

    /var/www is created as it has the error

--- ansible/roles/wordpress-3.8.1/files/install_wp.sh
+++ ansible/roles/wordpress-3.8.1/files/install_wp.sh
@@ -10,8 +10,13 @@ echo "unarchive it";
 tar xzf wordpress-3.8.1-ja.tar.gz
 
 echo "mv wordpress /var/www"
+if [[ ! -e '/var/www' ]]
+then
+    echo mkdir -p /var/www
+fi
+
 mv wordpress /var/www
-chown -R nginx.nginx /var/www/wordpress
+chown -R nginx.nginx /var/www
 
 echo "take backup of wp-config.php"
 cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
